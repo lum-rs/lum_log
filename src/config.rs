4743c8dcf415b06b63d4da8b1c523f7386f3a65b
Torben Schweren <torben@schweren.dev>
2024-11-01 11:50:55 +0100

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use lum_libs::{
+    log::LevelFilter,
+    serde::{Deserialize, Serialize},
+};
+
+/// Parts of the logger configuration that are meant to be user-configurable, and thus serializable and deserializable.
+/// This is used by the [`setup`](crate::setup) function and the [`Builder`](crate::Builder) to set up the logger.
+/// The idea is to implement `AsRef<Config>` for your own configuration type, and then use it to set up the logger.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub colors: HashMap<LevelFilter, String>,
+    pub min_log_level: LevelFilter,
+}
+
+impl Default for Config {
+    /// Returns a new instance of `Config` with the below default values.
+    /// This is the same as [`defaults::config()`](crate::defaults::config).
+    ///
+    /// ### Colors
+    /// | Level | Color  |
+    /// |-------|--------|
+    /// | Error | red    |
+    /// | Warn  | yellow |
+    /// | Info  | green  |
+    /// | Debug | magenta|
+    /// | Trace | cyan   |
+    ///
+    /// ### Minimum log level
+    /// `Info`
+    ///
+    /// # Example
+    /// ```
+    /// use lum_log::Config;
+    ///
+    /// let config = Config::default();
+    /// assert_eq!(config.colors.len(), 5);
+    /// assert_eq!(config.min_log_level, lum_libs::log::LevelFilter::Info);
+    /// ```
+    ///
+    fn default() -> Self {
+        let mut colors = HashMap::new();
+        colors.insert(LevelFilter::Error, "red".to_string());
+        colors.insert(LevelFilter::Warn, "yellow".to_string());
+        colors.insert(LevelFilter::Info, "green".to_string());
+        colors.insert(LevelFilter::Debug, "magenta".to_string());
+        colors.insert(LevelFilter::Trace, "cyan".to_string());
+
+        Config {
+            colors,
+            min_log_level: LevelFilter::Info,
+        }
+    }
+}
---
+++ src/defaults.rs
@@ -0,0 +1,42 @@
+use std::{
+    fmt::Arguments,
+    io::{self},
+    time::SystemTime,
+};
+
+use lum_libs::{
+    fern::{colors::ColoredLevelConfig, FormatCallback},
+    humantime,
+    log::Record,
+};
+
+use crate::Config;
+
+/// Returns a default `Config`.
+/// For more information, see [`Config::default`](crate::Config::default).
+pub fn config() -> Config {
+    Config::default()
+}
+
+/// Returns a vector containing [`io::stdout()`](std::io::stdout).
+/// Into [`fern::Dispatch`](lum_libs::fern::Dispatch) is implemented for Vec<io::Stdout>, so this can be used as a chain.
+pub fn chains() -> Vec<io::Stdout> {
+    vec![io::stdout()]
+}
+
+/// Returns a closure that formats the log message in the following format:
+/// ```text
+/// [2024-11-12T21:10:32Z example::module::path INFO ] This is a log message
+/// ```
+pub fn format(
+) -> impl Fn(FormatCallback, &Arguments, &Record, &ColoredLevelConfig) + Sync + Send + 'static {
+    move |out: FormatCallback, message: &Arguments, record: &Record, colors: &ColoredLevelConfig| {
+        out.finish(format_args!(
+            "[{} {: <30} {: <5}] {}",
+            humantime::format_rfc3339_seconds(SystemTime::now()),
+            record.target(),
+            colors.color(record.level()),
+            message
+        ))
+    }
+}
