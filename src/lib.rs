f34ee0636bbe02355409a1a85b9690dea4625645
Torben Schweren <torben@schweren.dev>
2024-11-18 20:43:59 +0100

    remove: setup_with_defaults()
    The original idea was for a developer to use setup_with_defaults() before initializing the actual logger. This way, there was a default logger implementation, and the developer could init their logger after loading user config files or similar. But a global logger in rust can only mit set once. Instead, this crate provides macros to log, which will fall back to using stdout/stderr when logging and no logger has been initialized yet. This way, the logging macros of this crate will always produce output at the cost of a bit of runtime performance.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use lum_libs::log::SetLoggerError;
-
 pub mod builder;
 pub mod config;
 pub mod defaults;
@@ -9,22 +7,3 @@ pub mod macros;
 pub use builder::Builder;
 pub use config::Config;
 pub use logger::{is_set_up, setup};
-
-/// Set up the logger using minimal configuration.
-/// This will use the defaults defined in [`defaults`].
-///
-/// # Example
-/// ```
-/// use lum_log::{is_set_up, setup_with_defaults};
-///
-/// assert!(!is_set_up());
-///
-/// let result = setup_with_defaults();
-///
-/// assert!(result.is_ok());
-/// assert!(is_set_up());
-pub fn setup_with_defaults() -> Result<(), SetLoggerError> {
-    Builder::new(defaults::format()).apply()?;
-
-    Ok(())
-}
